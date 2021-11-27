a018569a79b3537115b92e036ba97682239040b8
mubasher <mubashermajeed43@mail.com>
2021-11-27 16:33:34 +0500

    Web Socket

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,8 @@ import { Usersv2Module } from "./module/usersv2/usersv2.module";
 import { AuthModule } from "./data/utilities/auth/auth.module";
 import { ConfigModule } from "@nestjs/config";
 import { ScheduleModule } from "@nestjs/schedule";
-import { TaskModule } from "./data/utilities/task_scheduling/task/task.module";
 import { EventsModule } from "./web_scokets/events/events.module";
+import { TaskModule } from "./data/utilities/task_scheduling/task/task.module";
 
 @Module({
   imports: [MongooseModule.forRoot("mongodb+srv://mubashir:123@cluster0.crunx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"), UsersModule, VehiclesModule,
